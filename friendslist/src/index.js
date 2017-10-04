708b15c5e89f99147384f449201f5fc12368138c
AJ Hephner <ajhephner178@gmail.com>
2017-10-04 15:24:15 -0400

    done mod 4 Friends List Works

--- friendslist/src/index.js
+++ friendslist/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Header from './Header';
+import FriendList from './FriendList';
+import FriendForm from './FriendForm';
 import './index.css';
 
 ReactDOM.render(
     <div className="row">
         <App>
             <Header />
+            <FriendForm />
         </App>
     </div>,
     document.getElementById('root')
